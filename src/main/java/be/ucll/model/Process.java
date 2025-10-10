4ec7ebd48e8679dec5f8e320ac98d6f6dff38d29
zakariashahruriUCLL <zakaria.shahruri@student.ucll.be>
2025-10-10 09:55:34 +0200

    Feat[request_model]: Created the request model.

--- src/main/java/be/ucll/model/Process.java
+++ src/main/java/be/ucll/model/Process.java
@@ -6,5 +6,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "Process")
 public abstract class Process {
     @Id
-    public String id;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
