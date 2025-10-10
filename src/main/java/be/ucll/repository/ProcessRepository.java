17c6a16119cff8e017842b011b58f41f4e16a391
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2025-10-10 10:26:13 +0200

    adding import

--- src/main/java/be/ucll/repository/ProcessRepository.java
+++ src/main/java/be/ucll/repository/ProcessRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+import be.ucll.model.Process;
+
 public interface ProcessRepository extends MongoRepository<Process, String>{
     public Optional<Process> getById(String id);
 }
