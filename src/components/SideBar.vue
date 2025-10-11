657110b8187cc08a7921e09b1c303b9ef9ea486b
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2025-10-11 20:39:10 +0200

    Feat[create_home_page_ui]: optimized the sidebar component >1. now loops over a list to not needlessly repeat styling

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,72 +2,32 @@
 import { ref } from 'vue';
 
 const activeIndex = ref<number>(0);
+const menuItems = ['HOME', 'FLOW DEFINITIONS', 'FLOW INSTANCES', 'SETTINGS'];
 
-const selectedStyle = "bg-[var(--color-sidebarhighlight)] relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-2 before:bg-yellow-500"; // optional highlight variable (20% opacity)
-const notselected =
-  "relative px-[18px] py-[18px] pl-[24px] font-semibold cursor-pointer transition-colors duration-200 hover:bg-[#f5f5f533]";
+const yellowBar = 'before:content-[\'\'] before:absolute before:left-0 before:top-0 before:h-full before:w-2 before:bg-sidebarprimary';
 
-const setActive = (index: number) => {
-  activeIndex.value = index;
-};
+const selectOption = (index: number)=>{
+    activeIndex.value = index;
+
+    //TODO: route to url
+}
 </script>
 
 <template>
-  <aside
-    class="h-screen bg-[var(--color-sidebarbg)] py-5 shadow-[2px_0_5px_rgba(0,0,0,0.05)] flex flex-col items-center"
-  >
-    <img src="/images/logo.png" alt="Logo" class="w-24 mx-auto block" />
-    <hr class="text-[var(--color-sidebarsecondary)] mt-4 w-full" />
-
+  <aside class="h-screen bg-sidebarbg py-5 shadow-md flex flex-col items-center">
+    <img src="/images/logo.png" alt="Logo" class="w-24 mx-auto block"/>
+    <hr class="text-sidebarsecondary mt-4 w-full" />
+    
     <nav class="w-full">
-      <ul class="list-none mt-5">
+      <ul class="mt-5">
         <li
-          @click="setActive(0)"
-          :class="activeIndex == 0 ? selectedStyle + ' ' + notselected : notselected"
+          v-for="(item, index) in menuItems"
+          :key="index"
+          @click="selectOption(index)"
+          class="relative px-[18px] py-[18px] pl-6 font-semibold cursor-pointer transition-colors duration-200 hover:bg-[#f5f5f533]"
+          :class="activeIndex === index ? `${yellowBar} text-sidebarprimary bg-sidebarhighlight` : 'text-sidebarsecondary'"
         >
-          <a
-            href="#"
-            :class="activeIndex == 0 ? 'text-[var(--color-sidebarprimary)]' : 'text-[var(--color-sidebarsecondary)]'"
-            class="no-underline font-medium transition-colors duration-200"
-          >
-            HOME
-          </a>
-        </li>
-        <li
-          @click="setActive(1)"
-          :class="activeIndex == 1 ? selectedStyle + ' ' + notselected : notselected"
-        >
-          <a
-            href="#"
-            :class="activeIndex == 1 ? 'text-[var(--color-sidebarprimary)]' : 'text-[var(--color-sidebarsecondary)]'"
-            class="no-underline font-medium transition-colors duration-200"
-          >
-            FLOW DEFINITIONS
-          </a>
-        </li>
-        <li
-          @click="setActive(2)"
-          :class="activeIndex == 2 ? selectedStyle + ' ' + notselected : notselected"
-        >
-          <a
-            href="#"
-            :class="activeIndex == 2 ? 'text-[var(--color-sidebarprimary)]' : 'text-[var(--color-sidebarsecondary)]'"
-            class="no-underline font-medium transition-colors duration-200"
-          >
-            FLOW INSTANCES
-          </a>
-        </li>
-        <li
-          @click="setActive(3)"
-          :class="activeIndex == 3 ? selectedStyle + ' ' + notselected : notselected"
-        >
-          <a
-            href="#"
-            :class="activeIndex == 3 ? 'text-[var(--color-sidebarprimary)]' : 'text-[var(--color-sidebarsecondary)]'"
-            class="no-underline font-medium transition-colors duration-200"
-          >
-            SETTINGS
-          </a>
+          {{ item }}
         </li>
       </ul>
     </nav>
