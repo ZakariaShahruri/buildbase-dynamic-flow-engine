93ba74b3b60d2495ae16b04d520a80599535bba8
Moad <moad.khaouili@student.ucll.be>
2025-10-12 05:44:54 +0200

    Setup[adding_all_pages] : added routing. >1. added all necessary files and packages for the router, and tried to implement it the best way possible > 2. every page has its own path now > 3. modified the structure of the file, now the pages are in views not components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+  import FlowDefinitionForm from './views/FlowDefinitionForm.vue';
   import SideBar from './components/SideBar.vue';
-  import WelcomeGrid from './components/WelcomeGrid.vue';
+  import FlowDetails from './views/FlowDetails.vue';
+
 </script>
 
 <template>
@@ -9,7 +11,9 @@
       <SideBar />
     </div>
     <div class="w-full flex items-center justify-center">
-      <WelcomeGrid /> 
+      <div class="w-full p-5 m-5 border rounded-xl flex">
+        <router-view/>
+      </div>
     </div>
   </main>
 </template>
---
+++ src/views/WelcomeGrid.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+    const gridStyle = "bg-[#b9b9b9] h-50"
+</script>
+
+<template>
+  <div class="w-full p-5">
+    <p class="font-bold text-[2rem] pb-5">Welcome Manager</p>
+    <div class="flex flex-wrap gap-4">
+      <div class="w-[49%]">
+        <p>Flow Definitions</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Flow Instances</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Diagram</p>
+          <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Something</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
