1bae984806c0fddf200b2f3e3f3a47023a93fa82
Moad <moad.khaouili@student.ucll.be>
2025-10-12 02:20:56 +0200

    Feat[Home_UI]: Created Home page UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import SideBar from './components/SideBar.vue';
   import WelcomeGrid from './components/WelcomeGrid.vue';
+  
 </script>
 
 <template>
@@ -9,7 +10,9 @@
       <SideBar />
     </div>
     <div class="w-full flex items-center justify-center">
-      <WelcomeGrid /> 
+      <div class="w-full m-10 border rounded-xl">
+        <WelcomeGrid />
+      </div>
     </div>
   </main>
 </template>
--- src/components/WelcomeGrid.vue
+++ src/components/WelcomeGrid.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
-    const gridStyle = "bg-red-500 h-[10rem] w-[10rem]"
+    const gridStyle = "bg-[#b9b9b9] h-50"
 </script>
 
 <template>
-  <div class="grid grid-cols-2 w-full max-w-[50vw] gap-4">
-    <div :class="gridStyle">
-      <p>Hello1</p>
-    </div>
-    <div :class="gridStyle">
-      <p>Hello2</p>
-    </div>
-    <div :class="gridStyle">
-      <p>Hello3</p>
-    </div>
-    <div :class="gridStyle">
-      <p>Hello4</p>
+  <div class="w-full p-5">
+    <p class="font-bold text-[2rem] pb-5">Welcome Manager</p>
+    <div class="flex flex-wrap gap-4">
+      <div class="w-[49%]">
+        <p>Flow Definitions</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Flow Instances</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Diagram</p>
+          <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
+      <div class="w-[49%]">
+        <p>Something</p>
+        <a href="">
+          <div :class="gridStyle">
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
