ff6a9308b6298050c4878c292bf9d21421de18aa
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2025-10-12 13:45:17 +0200

    Feat[flow_models]: Updated the Flow domain classes >1. added Domain Exception and domain level validation checking >2. switched domain Ids to Mongo's ObjectIds instead of String >3. now extracting creation date from ObjectId rather than manually

--- src/main/java/be/ucll/model/FlowInstance.java
+++ src/main/java/be/ucll/model/FlowInstance.java
@@ -1,96 +1,96 @@
 package be.ucll.model;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import be.ucll.exception.DomainException;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Document(collection = "FlowInstance")
 public class FlowInstance {
 
     @Id
-    private String id;
-    private String title;
-    private String description;
+    private ObjectId id;
+    @NotNull
     private FlowDefinition flowDefinition;
+    @NotBlank
+    private String title;
+    @NotNull
+    private Status status;
+
     private Process currentProcess;
-    private List<Process> processes = new ArrayList<>();
-    private Date createdAt;
-    private Date updatedAt;
+    private LocalDate updatedAt;
 
-    public FlowInstance(String title, String description, Date createdAt, Process currentProcess, FlowDefinition flowDefinition, List<Process> processes, Date updatedAt) {
-        setTitle(title);
-        setDescription(description);
-        setCreatedAt(createdAt);
-        setCurrentProcess(currentProcess);
+    public FlowInstance(FlowDefinition flowDefinition, String title) {
         setFlowDefinition(flowDefinition);
-        setProcesses(processes);
-        setUpdatedAt(updatedAt);
+        setTitle(title);
+        setCurrentProcess(flowDefinition.getProcesses().get(0));
+        setStatus(Status.ACTIVE);
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Process getCurrentProcess() {
-        return currentProcess;
-    }
-
-    public void setCurrentProcess(Process currentProcess) {
-        this.currentProcess = currentProcess;
+    public ObjectId getId() {
+        return id;
     }
 
     public FlowDefinition getFlowDefinition() {
         return flowDefinition;
     }
 
-    public void setFlowDefinition(FlowDefinition flowDefinition) {
-        this.flowDefinition = flowDefinition;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    public Process getCurrentProcess() {
+        return currentProcess;
     }
 
     public List<Process> getProcesses() {
-        return processes;
+        return flowDefinition.getProcesses();
     }
 
-    public void setProcesses(List<Process> processes) {
-        this.processes = processes;
+    public Status getStatus() {
+        return status;
     }
 
-    public Date getUpdatedAt() {
+    public LocalDate getCreatedAt() {
+        if (id == null) 
+            throw new DomainException("Flow Instance not yet Created");
+
+        return id.getDate()
+            .toInstant()
+            .atZone(ZoneOffset.UTC)
+            .toLocalDate();
+    }
+
+    public LocalDate getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
+    public void setTitle(String title) {
+        if (title.isBlank()) 
+            throw new DomainException("FlowInstance Title cannot be empty");
+
+        this.title = title;
+    }
+
+    public void setCurrentProcess(Process currentProcess) {
+        this.currentProcess = currentProcess;
+    }
+
+    public void setFlowDefinition(FlowDefinition flowDefinition) {
+        if (flowDefinition == null) 
+            throw new DomainException("Flow Instance requires a definition");
+            
+        this.flowDefinition = flowDefinition;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
     }
 }
