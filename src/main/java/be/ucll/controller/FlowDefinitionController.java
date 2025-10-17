9302e1a786ab6db136ba50c024c76a30c9a2aaa4
Adam03lvl <adam.benkhazzi@outlook.com>
2025-10-17 14:41:20 +0200

    Feat[cors]: allow all origins

--- src/main/java/be/ucll/controller/FlowDefinitionController.java
+++ src/main/java/be/ucll/controller/FlowDefinitionController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flowDefinition")
+@CrossOrigin("*")
 public class FlowDefinitionController {
     private FlowDefinitionService flowDefinitionService;
 
