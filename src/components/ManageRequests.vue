af3657ce5c40155f6ffbcbf48bbf60fc6dea05b8
Moad <moad.khaouili@student.ucll.be>
2025-11-14 11:52:57 +0100

    fixed the background

--- src/components/ManageRequests.vue
+++ src/components/ManageRequests.vue
@@ -37,10 +37,10 @@ const closeModal = () => {
 </script>
 
 <template>
-  <div v-if="isClicked" class="absolute left-1/2 top-1/2 w-[70%] h-[70%] -translate-x-1/2 -translate-y-1/2 rounded-md border border-gray-300 bg-white shadow-lg ring-1 ring-gray-200 z-50">
+  <div v-if="isClicked" class="absolute left-1/2 top-1/2 w-[50%] h-[70%] -translate-x-1/2 -translate-y-1/2 rounded-md border border-gray-300 bg-white shadow-lg ring-1 ring-gray-200 z-50">
     <div class="p-6 h-full overflow-y-auto relative">
-      <button @click="closeModal" class="absolute right-4 top-4 rounded-md text-gray-600 hover:text-gray-900">✕</button>
-      <h2 class="text-2xl font-semibold mb-4">Manage this request</h2>
+      <button @click="closeModal" class="absolute right-4 top-4 rounded-md text-gray-600 cursor-pointer hover:text-gray-900">✕</button>
+      <h2 class="text-2xl font-semibold mb-4 text-center">Manage this request</h2>
 
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div v-for="(label, key) in {
@@ -52,20 +52,20 @@ const closeModal = () => {
           <div class="text-xs font-medium text-gray-500">{{ key }}</div>
           <div class="mt-1 text-lg font-semibold text-gray-800">{{ label || 'no value' }}</div>
         </div>
-
-        <div class="sm:col-span-2 bg-gray-50 border border-gray-200 rounded-md p-4">
+      </div>
+      <div class="w-full pt-4 flex justify-center">
+        <div class="sm:col-span-2 bg-gray-50 border border-gray-200 rounded-md p-4 w-[60%]">
           <div class="text-xs font-medium text-gray-500">Reason</div>
           <div class="mt-1 text-lg font-semibold text-gray-800">{{ selectedRequest?.data?.allFields?.reason || 'no value' }}</div>
         </div>
       </div>
-
       <div class="absolute bottom-5 w-[93%] flex justify-center gap-5">
         <button @click="handleApproveRequest" class="mt-6 px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700">Approve</button>
         <button @click="handleDeclineRequest" class="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700">Decline</button>
       </div>
     </div>
   </div>
-
+  <div v-if="isClicked" class="h-screen w-screen absolute left-0 top-0 bg-black opacity-50"></div>
   <div class="w-full rounded-md p-5">
     <div class="rounded-lg border bg-white p-6 border-gray-300 shadow-sm">
       <h2 class="font-bold text-4xl py-5">Pending Requests:</h2>
