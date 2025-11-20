b4286d1693e2c4400fb28c6b96c6091c961d2576
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2025-11-20 17:33:22 +0100

    Feat[CORS]: Added global cors

--- src/main/java/be/ucll/controller/ProcessController.java
+++ src/main/java/be/ucll/controller/ProcessController.java
@@ -3,7 +3,6 @@ package be.ucll.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import be.ucll.service.ProcessService;
 
 @RestController
 @RequestMapping("/process")
-@CrossOrigin(origins = "*")
 public class ProcessController {
   
   @Autowired
--- src/main/java/be/ucll/controller/RequestController.java
+++ src/main/java/be/ucll/controller/RequestController.java
@@ -3,7 +3,6 @@ package be.ucll.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +16,6 @@ import be.ucll.service.RequestService;
 
 @RestController
 @RequestMapping("/request")
-@CrossOrigin(origins = "*")
 public class RequestController {
 
     @Autowired
