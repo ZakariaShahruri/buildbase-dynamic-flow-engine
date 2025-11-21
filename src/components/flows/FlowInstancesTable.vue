d2c08c51448013d539d6d740ef1c542e5c80c824
sqmmi3 <samip.shrestha@student.ucll.be>
2025-11-21 14:30:57 +0100

    FIX [dev]: colour changes to status circle

--- src/components/flows/FlowInstancesTable.vue
+++ src/components/flows/FlowInstancesTable.vue
@@ -175,11 +175,11 @@ onMounted(() => {
             <span
               class="inline-block w-3 h-3 rounded-full flex-shrink-0"
               :class="{
-                'bg-teal-500': inst.flowStatus.toLowerCase() === 'active',
-                'bg-green-500': inst.flowStatus.toLowerCase() === 'success',
-                'bg-orange-500': inst.flowStatus.toLowerCase() === 'pending',
-                'bg-red-500': inst.flowStatus.toLowerCase() === 'failed',
-                'bg-blue-500': inst.flowStatus.toLowerCase() === 'paused',
+                'bg-[#10b981]': inst.flowStatus.toLowerCase() === 'active',
+                'bg-[#84cc16]': inst.flowStatus.toLowerCase() === 'success',
+                'bg-[#f59e0b]': inst.flowStatus.toLowerCase() === 'pending',
+                'bg-[#ef4444]': inst.flowStatus.toLowerCase() === 'failed',
+                'bg-[#3b82f6]': inst.flowStatus.toLowerCase() === 'paused',
               }"
             ></span>
             <span class="text-gray-900 font-medium">{{
