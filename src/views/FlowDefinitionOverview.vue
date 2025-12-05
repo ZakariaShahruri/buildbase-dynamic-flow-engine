41e45c6c77742a1c0d2ee2a5b39fb3039090ee6e
Angelo Mouawad <angelo.mouawad@gmail.com>
2025-12-05 16:44:24 +0100

    Finalized user and manager seperation

--- src/views/FlowDefinitionOverview.vue
+++ src/views/FlowDefinitionOverview.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
   import FlowTypeCreation from '../components/flows/FlowTypeCreation.vue';
+  import { ref } from 'vue';
+  import router from '../router';
+
+  const user = ref<any>(null)
+  const stored = sessionStorage.getItem("user")
+
+  if (stored) {
+    user.value = JSON.parse(stored)
+  }
+
+  if (user.value?.role == "User") {
+    router.push({ name: "Home" });
+  }
 </script>
 
 <template>
