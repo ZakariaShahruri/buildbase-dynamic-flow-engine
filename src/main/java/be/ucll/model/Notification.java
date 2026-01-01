4f3b28c9a75c3c69db3cd13d738b5d1ff5b53898
sqmmi3 <samip.shrestha@student.ucll.be>
2026-01-01 18:19:06 +0100

    FIX [notification]: 1. small code improvements for notificationTypeName and notificationType assignment

--- src/main/java/be/ucll/model/Notification.java
+++ src/main/java/be/ucll/model/Notification.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import be.ucll.model.enums.NotificationTypeEnum;
 import be.ucll.model.enums.ProcessType;
@@ -14,22 +15,28 @@ public class Notification extends Process {
 
     @JsonIgnore
     private NotificationType notificationType;
+
+    @JsonProperty("notificationType")
     private NotificationTypeEnum notificationTypeName;
 
     private List<String> toNotify = new ArrayList<>();
     private int requestStep;
+
+    @JsonIgnore
     private Request request;
+
+    public Notification() { super(); }
 
     public Notification(String name, NotificationTypeEnum notificationTypeName, List<String> toNotify, int requestStep) {
         super(name, ProcessType.NOTIFICATION);
-        this.notificationTypeName = notificationTypeName;
-        setNotificationType(notificationTypeName);
+        setNotificationTypeName(notificationTypeName);
+        // setNotificationType(notificationTypeName);
         setToNotify(toNotify);
         this.requestStep = requestStep;
     }
 
     public List<String> getToNotify() {
-        if (toNotify == null) {
+        if ((toNotify == null || toNotify.isEmpty()) && this.request != null) {
             return List.of(this.request.getApprovableBy());
         }
 
@@ -63,7 +70,12 @@ public class Notification extends Process {
         this.toNotify = toNotify;
     }
 
-    public void setNotificationType(NotificationTypeEnum notificationTypeName) {
-        this.notificationType = NotificationTypeFactory.fromTypeName(notificationTypeName);
+    @JsonProperty("notificationType")
+    public void setNotificationTypeName(NotificationTypeEnum notificationTypeName) {
+        this.notificationTypeName = notificationTypeName;
+        if (notificationTypeName != null) {
+            this.notificationType = NotificationTypeFactory.fromTypeName(notificationTypeName);
+        }
     }
+
 }
